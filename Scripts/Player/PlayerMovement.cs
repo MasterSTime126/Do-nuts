fe97b0ab9da0a8e80e8a240aea82805bbc187636
MasterSTime126 <hello.neinbor@gmail.com>
2026-01-27 01:10:15 +0500

    player move+init

---
+++ Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float currentSpeed = 5f;
+    private float moveSpeed = 5f;
+    private float sprintSpeed = 7.5f;
+
+   private Vector2 movementInput;
+   private InputActionAsset inputActions;
+   private InputActionMap playerActionMap;
+   private InputAction moveAction;
+   private InputAction sprintAction;
+
+   private void OnEnable()
+   {
+       inputActions = GetComponent<PlayerInput>().actions;
+       playerActionMap = inputActions.FindActionMap("Player");
+       moveAction = playerActionMap.FindAction("Move");
+       sprintAction = playerActionMap.FindAction("Sprint");
+       moveAction.performed += ctx => movementInput = ctx.ReadValue<Vector2>();
+       moveAction.canceled += ctx => movementInput = Vector2.zero;
+       playerActionMap.Enable();
+   }
+
+   private void Update()
+   {
+       currentSpeed = sprintAction.IsPressed() ? sprintSpeed : moveSpeed;
+       Vector3 move = new Vector3(movementInput.x, 0, movementInput.y) * currentSpeed * Time.deltaTime;
+       transform.Translate(move);
+   }
+}
